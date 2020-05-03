ceaaf98ce28c408492f7071e85c453db79473b30
Victor Aquiles <victoraqalencar@gmail.com>
2020-05-03 14:56:53 -0300

    Component de Categoria com gráfico de progresso

--- screens/VisaoGeralScreen.js
+++ screens/VisaoGeralScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text, Button } from "react-native";
 import { IndicesNavegacao } from "../navigation/IndicesNavegacao";
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { FlatList } from "react-native-gesture-handler";
+import { ProgressCircle } from 'react-native-svg-charts'
 
 const ItemRenda = ({ item }) => {
 
@@ -34,6 +35,79 @@ const ItemRenda = ({ item }) => {
     )
 }
 
+const geraDadosCategoria = (userData, gastos) => {
+
+    let categoryData = {}
+
+    // inicializando dados de cada categoria
+    Object.entries(userData).forEach(([key, value]) => {
+        categoryData[key] = { renda: value, gasto: 0.0 }
+    })
+
+    // atribuindo valor de cada item a sua respectiva categoria
+    gastos.map(item => {
+        if (item.tipo === 0) {
+            categoryData[item.categoria].gasto += item.valor
+        }
+    })
+
+    // reorganizando dict para uma lista de dicts
+    let categoryArray = []
+    Object.entries(categoryData).forEach(([key, value]) => {
+        categoryArray.push({ ...value, categoria: key })
+    })
+
+    return categoryArray
+}
+
+const Categoria = ({ item }) => {
+
+    const styles = StyleSheet.create({
+        item: {
+            flex: 1,
+            flexDirection: "row",
+            backgroundColor: '#F0F0F0',
+            paddingTop: 10,
+            paddingBottom: 10,
+            marginVertical: 8,
+            alignItems: "center"
+        },
+        categoria: {
+            flex: 2,
+            textAlign: "left",
+            fontSize: 18
+        },
+        gasto: {
+            flex: 2,
+            textAlign: "right",
+            fontSize: 14,
+            paddingRight: 10
+      },
+        chart: {
+            flex: 1,
+            alignItems: "baseline",
+            height: 40
+        }
+    })
+
+    const percentage = parseFloat(item.gasto) / parseFloat(item.renda)
+
+    return (
+        <View style={styles.item}>
+            <ProgressCircle
+                style={styles.chart}
+                progress={percentage}
+                progressColor={'rgb(134, 65, 244)'} />
+
+            <Text style={styles.categoria}>{item.categoria}</Text>
+            <Text style={styles.gasto}>
+                $ {item.gasto} /
+                <Text style={{ fontWeight: "bold" }}> $ {item.renda}</Text>
+            </Text>
+        </View>
+    )
+}
+
 const VisaoGeralScreen = (props) => {
 
     const [showRenda, setShowRenda] = useState(true)
@@ -55,6 +129,14 @@ const VisaoGeralScreen = (props) => {
         }
     }
 
+    const userData = {
+        casa: 900.00,
+        educacao: 150.00,
+        transporte: 150.00,
+        lazer: 100.00,
+        alimentacao: 400.00
+    }
+
     const data = [
         {
             valor: 80.00,
@@ -64,10 +146,38 @@ const VisaoGeralScreen = (props) => {
             data: "05-02-2020"
         },
         {
+            valor: 100.00,
+            descricao: "Coach",
+            tipo: 0,
+            categoria: "educacao",
+            data: "05-02-2020"
+        },
+        {
+            valor: 100.00,
+            descricao: "Japones do Cleber",
+            tipo: 0,
+            categoria: "alimentacao",
+            data: "05-02-2020"
+        },
+        {
+            valor: 85.50,
+            descricao: "Mais uma Skin da Lux",
+            tipo: 0,
+            categoria: "lazer",
+            data: "05-02-2020"
+        },
+        {
+            valor: 84.50,
+            descricao: "Uber",
+            tipo: 0,
+            categoria: "transporte",
+            data: "05-02-2020"
+        },
+        {
             valor: 1800.00,
             descricao: "Bilhetes de Jogo do Bicho",
             tipo: 0,
-            categoria: "educacao",
+            categoria: "lazer",
             data: "05-02-2020"
         },
         {
@@ -100,10 +210,10 @@ const VisaoGeralScreen = (props) => {
             {/* Aba de Renda */}
             <View style={{ flex: 1 }}>
                 <View style={styles.flatlist}>
-                    <Text style={{marginLeft: 10}}>Renda</Text>
+                    <Text style={{ marginLeft: 10 }}>Renda</Text>
 
-                    <View style={styles.lineStyle}/>
-                    
+                    <View style={styles.lineStyle} />
+
                     {showRenda ?
                         (<Icon.Button name='angle-up' color="black" backgroundColor='#ffff' onPress={ShowHideRenda} />)
                         : (<Icon.Button name='angle-down' color="black" backgroundColor='#ffff' onPress={ShowHideRenda} />)
@@ -123,12 +233,12 @@ const VisaoGeralScreen = (props) => {
                 ) : null}
             </View>
 
-            {/* Aba de Gastos */}
-            <View style={{ flex: 1 }}>
+            {/* Aba de Despesas */}
+            <View style={{ flex: 2 }}>
                 <View style={styles.flatlist}>
-                    <Text style={{marginLeft: 10}}>Gastos</Text>
-                    
-                    <View style={styles.lineStyle}/>
+                    <Text style={{ marginLeft: 10 }}>Despesas</Text>
+
+                    <View style={{ ...styles.lineStyle, width: "64%" }} />
 
                     {showGastos ?
                         (<Icon.Button name='angle-up' color="black" backgroundColor='#ffff' onPress={ShowHideGastos} />)
@@ -138,12 +248,8 @@ const VisaoGeralScreen = (props) => {
 
                 {showGastos ? (
                     <FlatList
-                        data={data}
-                        renderItem={({ item }) => {
-                            if (item.tipo === 0) {
-                                return <ItemRenda item={item} />
-                            }
-                        }}
+                        data={geraDadosCategoria(userData, data)}
+                        renderItem={({ item }) => <Categoria item={item} />}
                         keyExtractor={({ item, index }) => index}
                     />
                 ) : null}
